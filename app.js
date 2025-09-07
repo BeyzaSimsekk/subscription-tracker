df1172b0e936313223fcca522f936475e279b37b
Beyza <u.beyza.simsek@gmail.com>
2025-09-07 21:56:45 +0300

    config

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from "express";
 
+import {PORT} from "./config/env.js";
+
 const app = express(); //after this you can create routes
 
 //first parameter is the path, second is the callback
@@ -7,8 +9,8 @@ app.get('/', (req, res)=>{
     res.send('Welcome to the Subscription Tracker API!');
 });
 
-app.listen(3000, ()=>{
-    console.log('Subscription Tracker API is running on http://localhost:3000');
+app.listen(PORT, ()=>{
+    console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
 });
 
 export default app;
