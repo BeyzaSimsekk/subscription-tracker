a97364d24e4af589b02cfd3ba1dc196686844ef8
Beyza <u.beyza.simsek@gmail.com>
2025-09-16 15:04:01 +0300

    userSchema

---
+++ models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String, 
+        required:[true,"User name is required"], //[required, (error)message]
+        trim: true, //removes spaces
+        minLength: 2,
+        maxLength: 50,
+    }, 
+    email:{
+        type: String,
+        required: [true, "User email is required"],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        //start w/ a string + @ + string + . + string
+        match: [/\S+@\S+\.\S+/, "Please use a valid email address"], //u.beyza.simsek@gmail.com
+    },
+    password: {
+        type: String,
+        required: [true, "User password is required"],
+        minLength: 6
+    }
+},{timestamps: true}); //created_at, updated_at columns will be added
