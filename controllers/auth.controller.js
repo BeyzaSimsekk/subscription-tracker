d7a91376369dd6e5e930070b804e4d0d22b6177b
Beyza <u.beyza.simsek@gmail.com>
2025-09-21 19:29:04 +0300

    clean auth route

---
+++ controllers/auth.controller.js
@@ -0,0 +1,7 @@
+export const signUp = async (req, res, next) => {
+    // Implement the sign-up logic
+}
+
+export const signIn = async (req, res, next) => {}
+
+export const signOut = async (req, res, next) => {}
