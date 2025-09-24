0d7067c59b0f278bfdb5ca2eeddffae14c123766
Beyza <u.beyza.simsek@gmail.com>
2025-09-24 20:48:23 +0300

    Create auth.middleware.js

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,3 @@
+/**PURPOSE/DEFINITON
+ * some routes such as user details should be private, so we need to check if the user is authenticated or not. For this, we create a middleware that will check if the user is authenticated or not.
+ *  */  
