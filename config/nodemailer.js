31f915a917214f0ce924a8b4966f7e95c8f373db
Beyza <u.beyza.simsek@gmail.com>
2025-09-30 22:03:09 +0300

    send email

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,10 +1,12 @@
 import nodemailer from 'nodemailer'
 import { EMAIL_PASSWORD } from './env.js'
 
+export const accountEmail = 'u.beyza.simsek@gmail.com';
+
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'u.beyza.simsek@gmail.com',
+        user: accountEmail,
         pass: EMAIL_PASSWORD
     }
 })
